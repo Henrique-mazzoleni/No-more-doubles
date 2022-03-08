8eef2b0e65fc3f55298bb3f4810cd370b765d3c5
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-03-08 10:35:04 +0100

    sucesfull extraction of all favorites

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const qs = require("qs");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const app = require("express")();
-const generateRandomString = require("./utils/utils");
+const { generateRandomString, extractTracks } = require("./utils/utils");
 const hbs = require("hbs");
 
 const port = process.env.PORT || 8888;
@@ -21,12 +21,28 @@ app.use(cors()).use(cookieParser());
 app.get("", async (req, res) => {
   if (req.query?.access_token) {
     const access_token = req.query.access_token;
-
     const options = {
       headers: { Authorization: "Bearer " + access_token },
     };
-    const response = await axios.get("https://api.spotify.com/v1/me", options);
-    console.log(response.data);
+
+    const tracks = [];
+    let offsetLimit = 1;
+    let offset = 0;
+    try {
+      while (offset < offsetLimit) {
+        const response = await axios.get(
+          `https://api.spotify.com/v1/me/tracks?offset=${offset}&limit=50`,
+          options
+        );
+        if (offsetLimit === 1) offsetLimit = response.data.total;
+        tracks.push(...extractTracks(response.data.items));
+        offset += 50;
+        console.log(tracks.length);
+      }
+      console.log(tracks.slice(2820));
+    } catch (error) {
+      console.log(error);
+    }
   }
   res.render("index");
 });
@@ -34,7 +50,8 @@ app.get("", async (req, res) => {
 app.get("/login", (req, res) => {
   const state = generateRandomString(16);
   res.cookie(stateKey, state);
-  const scope = "user-read-private user-read-email";
+  const scope =
+    "user-read-private user-read-email user-library-read playlist-read-private user-top-read";
   const auth_url = "https://accounts.spotify.com/authorize?";
   const searchParams = {
     client_id: client_id,
@@ -54,26 +71,26 @@ app.get("/callback", async (req, res) => {
   const storedState = req.cookies ? req.cookies[stateKey] : null;
 
   if (state === null || state !== storedState) {
-    const params = new URLSearchParams({ error: state_missmatch });
-    res.redirect("/#" + params.toString());
+    const params = new URLSearchParams({ error: "state_missmatch" });
+    res.redirect("/?" + params.toString());
   } else {
     res.clearCookie(stateKey);
+    const data = qs.stringify({
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: redirect_uri,
+    });
+    const config = {
+      headers: {
+        Authorization:
+          "Basic " +
+          Buffer.from(client_id + ":" + client_secret, "utf8").toString(
+            "base64"
+          ),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    };
     try {
-      const data = qs.stringify({
-        grant_type: "authorization_code",
-        code: code,
-        redirect_uri: redirect_uri,
-      });
-      const config = {
-        headers: {
-          Authorization:
-            "Basic " +
-            Buffer.from(client_id + ":" + client_secret, "utf8").toString(
-              "base64"
-            ),
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      };
       const response = await axios.post(
         "https://accounts.spotify.com/api/token",
         data,
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -9,4 +9,18 @@ const generateRandomString = (length) => {
   return text;
 };
 
-module.exports = generateRandomString;
+const extractTracks = (list) => {
+  const res = [];
+  list.forEach((track) => {
+    res.push({
+      name: track.track.name,
+      artist: track.track.artists[0].name,
+      album: track.track.album.name,
+      added_at: track.added_at,
+    });
+  });
+
+  return res;
+};
+
+module.exports = { generateRandomString, extractTracks };
